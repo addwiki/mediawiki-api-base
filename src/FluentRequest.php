8f9e30a69f8115e2541d4dbdb4d7ad69c842becf
addshore <addshorewiki@gmail.com>
2015-08-23 15:00:30 +0200

    Add more methods to FluentRequest

--- src/FluentRequest.php
+++ src/FluentRequest.php
@@ -31,7 +31,7 @@ class FluentRequest implements Request {
 	 * @return $this
 	 */
 	public function setAction( $action ) {
-		$this->params['action'] = $action;
+		$this->setParam( 'action', $action );
 		return $this;
 	}
 
@@ -44,8 +44,35 @@ class FluentRequest implements Request {
 	 *
 	 * @return $this
 	 */
-	public function setParams( $params ) {
+	public function setParams( array $params ) {
 		$this->params = $params;
+		return $this;
+	}
+
+	/**
+	 * Totally overwrite any previously set params
+	 *
+	 * @since 1.0
+	 *
+	 * @param array $params
+	 *
+	 * @return $this
+	 */
+	public function addParams( array $params ) {
+		$this->params = array_merge( $this->params, $params );
+		return $this;
+	}
+
+	/**
+	 * @since 1.0
+	 *
+	 * @param string $param
+	 * @param string $value
+	 *
+	 * @return $this
+	 */
+	public function setParam( $param, $value ) {
+		$this->params[$param] = $value;
 		return $this;
 	}
 
